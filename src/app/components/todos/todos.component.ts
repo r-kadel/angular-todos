8b4324097507410f17610edc42a75f5d75be013c
Ryan Kadel <ryan.kadel88@gmail.com>
2020-06-15 13:29:10 -0400

    init

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit(): void {
+    this.todos = this.todoService.getTodos();
+  }
+
+}
