66b9ccd3739b423fa7ba9e608e7656fc17878de8
Ryan Kadel <ryan.kadel88@gmail.com>
2020-06-15 14:47:52 -0400

    80% complete, issues with the API

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id:1,
-        title: 'Todo 1',
-        completed: false
-      },
-      {
-        id:2,
-        title: 'Todo 2',
-        completed: true
-      },
-      {
-        id:3,
-        title: 'Todo 3',
-        completed: false
-      }
-    ]
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
+  }
+
+  deleteTodo(todo:Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
+  addTodo(todo:Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions)
   }
 }
